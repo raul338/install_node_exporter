28f5ec5842fef9b36b6dbef69588146af5b5ce31
Raúl Arellano <raul338@elhacker.net>
2020-03-14 23:14:35 -0300

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # install_node_exporter
-Simple script to install node_exporter in a vps
+Simple script to install `node_exporter` in a vps to `/srv/node_exporter` and activate it using `supervisor`
+
+To edit path or node_exporter version, update files accordingly
+
+```sh
+sudo apt update
+sudo apt install supervisor
+
+# If you cloned/downloaded this repo:
+sudo cp node_exporter.conf /etc/supervisor/conf.d
+sudo ./install_node_exporter.sh
+```
---
+++ install_node_exporter.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+version=0.18.1
+folder=/srv/node_exporter
+
+tmpfile="$folder/node_exporter.tar.gz"
+
+mkdir -p "$folder"
+if [ -f $tmpfile ]; then
+	gzip -t $tmpfile 2>1& > /dev/null
+	if [ ! $0 -eq 0 ]; then
+		rm $tmpfile
+	fi
+fi
+wget -O $tmpfile "https://github.com/prometheus/node_exporter/releases/download/v${version}/node_exporter-${version}.linux-amd64.tar.gz"
+tar -xf $tmpfile -C $folder
+if [ -f $folder/node_exporter ]; then
+	rm $folder/node_exporter
+fi
+ln "$folder/node_exporter-${version}.linux-amd64/node_exporter" $folder/node_exporter
+rm $tmpfile
+supervisorctl restart node_exporter
+
